5612f4d9a0de520b7f8eaa4a636f5b31b0af5fb4
lunksana <zoufeng4@gmail.com>
2021-12-27 14:31:32 +0800

    20211227

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
+# 库存系统数据库使用mongodb
+# 使用pymongo对mongodb进行操作
+# 每个库房对应一个数据库，相关的数据内容在相应的数据包中添加
+
 import pymongo
 import json
 
 # 读取用户配置文件
-if "conf/config.json":
-    with open('conf/config.json', 'r') as conf:
-        config = json.load(conf)
-else:
-    exit("config.json not found")
+with open('conf/config.json', 'r') as conf:
+    config = json.load(conf)
 
 # 基于配置文件信息判断是否已经进行初始化
 if config['init'] != 1:
@@ -15,15 +16,19 @@ if config['init'] != 1:
 else:
     addr = config['db_addr']
     port = config['db_port']
-    name = config['db_name']
 
 
 # 对数据库进行操作
 class mgdb:
-    def __init__(self,table):
+    def __init__(self, db_name, col_name):
         self.client = pymongo.MongoClient(addr, port)
-        self.db = self.client[name]
-        self.col = self.db[table]
+        self.db = self.client[db_name]
+        self.col = self.db[col_name]
+    
+    # 新建数据库
+    def new(self, db_name):
+        self.client.create_database(db_name)
+    
         
     # 查询数据
     def find(self, condition):
